cfabd98f86b191c91ff469e3fdd401560fa6df27
Destiny <59299158+yuheng14@users.noreply.github.com>
2022-05-06 14:37:18 +0800

    Update ofdm_PAPR.m

--- SIMULATION/ofdm_PAPR.m
+++ SIMULATION/ofdm_PAPR.m
@@ -1,3 +1,15 @@
 % OFDM signal's PAPR simulation
-% randi([min,max],m,n)    generate m*n matrix in [min,max]
+% along with:
+% PAPR.m(function to calculate CCDF)
+% mapper.m(function to constellation mapping)
+
+% matlab function used:
+% randi([min,max],m,n)        generate m*n matrix in [min,max]
+% lteSymbolModulate(in,mod)   map the bit value,in,to complex symbols
+% pskmod(x,M,phaseoffset)     secify the phase offset of the M-PSK constellation
 clear all;close all;clc
+
+
+
+
+
